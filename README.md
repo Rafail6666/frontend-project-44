62e402a89d7b20ee06c10237f5a68604960a73fb
Rafael Akhmetshin <ahmetshin.rafael@list.ru>
2023-09-16 22:13:57 +0500

    задание №5

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Rafail6666/frontend-project-44/workflows/hexlet-check/badge.svg)](https://github.com/Rafail6666/frontend-project-44/actions)
+
+
+[Игра: "Проверка на чётность"](https://asciinema.org/a/KlBU18upabA0rYwSfRXCGtQSq)
---
+++ bin/brain-even.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function main() {
+    console.log('Welcome to the Brain Games!')
+
+    const name = readlineSync.question('May I have your name? ');
+
+    console.log(`Hello, ${name}!`);
+
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+    for (let i = 0; i < 3; i++) {
+
+        const number = getRandomInt(10000);
+
+        console.log(`Question: ${number}!`)
+
+        const answer = readlineSync.question('Your answer: ');
+
+        if (number % 2 == 0) {
+            if (answer == "yes") {
+                console.log("Correct!")
+            } else {
+                console.log(`'${answer}' is wrong answer ;(. Correct answer was 'yes'.)`)
+                console.log(`Let's try again, ${name}!`)
+                return
+            }
+        } else {
+            if (answer == "no") {
+                console.log("Correct!")
+            } else {
+                console.log(`'${answer}' is wrong answer ;(. Correct answer was 'no'.)`)
+                console.log(`Let's try again, ${name}!`)
+                return
+            }
+        }
+    }
+
+    console.log(`Congratulations, ${name}!`)
+
+}
+
+main();
+
+
+
